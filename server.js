42822c46c3c09a176109ab66d9636342def1ba4f
Mila Decker <milacdecker@gmail.com>
2021-02-01 21:54:47 -0500

    added login authentication

--- server.js
+++ server.js
@@ -9,6 +9,23 @@ const PORT = process.env.PORT || 3001;
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+require('dotenv').config();
+
+const sess = {
+    secret: process.env.DB_SECRET,
+    cookie: {}, 
+    resave: false,
+    saveUnitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
