ea4a52d1da6ca3f40639d93db91d658dbaef1146
Mila Decker <milacdecker@gmail.com>
2021-02-02 15:30:31 -0500

    added single post page

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -93,13 +93,15 @@ router.post('/', (req, res) => {
 
 // put votes
 router.put('/upvote', (req, res) => {
-    // custom static method created in models/Post.js
-    Post.upvote(req.body, { Vote })
-        .then(updatedPostData => res.json(updatedPostData))
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
+    if (req.session) {
+        // pass session id along with all destructured properties on req.body
+        Post.upvote( { ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+            .then(updatedVoteData => res.json(updatedVoteData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    }
 });
 
 // update post title
