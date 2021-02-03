5d7cf6b092227dd71f19cad3f980dc5c9cc316be
Mila Decker <milacdecker@gmail.com>
2021-02-02 23:00:04 -0500

    added dashboard

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,7 @@
 const sequelize = require('../../config/connection');
 const router = require('express').Router();
 const { Post, User, Vote, Comment } = require('../../models');
+const withAuth = require('../../utils/auth')
 
 // get all posts
 router.get('/', (req, res) => {
@@ -78,11 +79,11 @@ router.get('/:id', (req, res) => {
 });
 
 // create post
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
     Post.create({
         title: req.body.title,
         post_url: req.body.post_url,
-        user_id: req.body.user_id  
+        user_id: req.session.user_id  
     })
         .then(dbPostData => res.json(dbPostData))
         .catch(err => {
@@ -92,7 +93,7 @@ router.post('/', (req, res) => {
 });
 
 // put votes
-router.put('/upvote', (req, res) => {
+router.put('/upvote', withAuth, (req, res) => {
     if (req.session) {
         // pass session id along with all destructured properties on req.body
         Post.upvote( { ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
@@ -105,7 +106,7 @@ router.put('/upvote', (req, res) => {
 });
 
 // update post title
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
     Post.update({
         title: req.body.title
     },
@@ -128,7 +129,7 @@ router.put('/:id', (req, res) => {
 });
 
 // delete a post
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     Post.destroy({
         where: {
             id: req.params.id
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Vote, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
     // access user model and run .findAll() method
@@ -140,7 +141,7 @@ router.put('/:id', (req, res) => {
 });
 
 // DELETE /api/users/1
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     User.destroy({
         where: {
             id: req.params.id
